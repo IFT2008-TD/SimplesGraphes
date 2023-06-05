53496cbde6cebff91f2cabf83d4528ad4262667d
Pascal Charpentier <59535994+pascalcharp@users.noreply.github.com>
2023-06-05 10:13:54 -0400

    Documentation du dispositif de test

--- tests/GrapheTest.h
+++ tests/GrapheTest.h
@@ -8,6 +8,20 @@
 #include "Graphe.h"
 #include "gtest/gtest.h"
 
+/**
+ * @class GrapheTest
+ *
+ * Dispositif de test servant à mettre à l'épreuve nos algorithmes de graphe.  Il contient quelques graphes
+ * préconstruits:
+ *
+ * g0 graphe vide
+ * g1 graphe à un seul sommet
+ * g2 graphe à deux sommets: 0 ---> 1
+ * g3 graphe à trois sommets: 0 ---> 1 ---> 2
+ * g6 graphe comportant 6 sommets.  Il y a un cycle 012, et un cycle 345 reliés par une arête 2->3.  Ce graphe a donc
+ * deux composantes fortement connexes: {0, 1, 2} et {3, 4, 5}.
+ */
+
 class GrapheTest : public ::testing::Test {
 protected:
     GrapheTest() : g0(0), g1(1), g2(2), g3(3), g6(6) {}
